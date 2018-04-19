b6c9696608058765cd2a8aada540467426bd86c1
vtran11 <vy.t.tran@sjsu.edu>
2018-04-18 19:15:44 -0700

    Finished UI Design + gMap on main Screen

--- app/src/main/java/vytran/cs175/sjsu/alphafitnessandroidapp/PortraitRecordWorkout.java
+++ app/src/main/java/vytran/cs175/sjsu/alphafitnessandroidapp/PortraitRecordWorkout.java
@@ -1,29 +1,45 @@
 package vytran.cs175.sjsu.alphafitnessandroidapp;
 
+import android.*;
+import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.LocationManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.Button;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+
+import android.Manifest;
+import android.net.Uri;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.MapView;
+
+
+import android.location.Location;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 
 public class PortraitRecordWorkout extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private MapView mapView;
+    private Context context;
+    private boolean isRunning = false;
+    Button workoutButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -35,8 +51,10 @@ public class PortraitRecordWorkout extends Fragment implements OnMapReadyCallbac
         mapView .onResume();
         mapView .getMapAsync(this);
 
+        workoutButton = (Button) portraitView.findViewById(R.id.RecordButton);
 
-        //-------set click listener for User Profile Button---------
+
+                //-------set click listener for User Profile Button---------
         ImageButton userProfileButton = (ImageButton) portraitView.findViewById(R.id.userProfileID);
         userProfileButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,17 +67,30 @@ public class PortraitRecordWorkout extends Fragment implements OnMapReadyCallbac
         return portraitView;
     }
 
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        //get the best location provider
-        LocationManager locationManager;
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        
-        // Add a marker in Sydney and move the camera
+
+       // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+
+    public void buttonStartWorkoutCLicked(View view){
+        if(!isRunning)
+        {
+            workoutButton.setText("Stop Workout");
+            isRunning = true;
+        }
+
+        else
+        {
+            workoutButton.setText("Start Workout");
+            isRunning= false;
+        }
     }
 }
